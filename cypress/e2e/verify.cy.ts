b0d256c632e0129b6c9c57898b48679ef34d1dc7
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-27 23:18:45 +0400

    fix: auto send verify request on page load

--- cypress/e2e/verify.cy.ts
+++ cypress/e2e/verify.cy.ts
@@ -3,15 +3,8 @@ describe('Verify', () => {
   const signature = '0x0000';
   const address = '0x1111';
 
-  beforeEach(() => {
-    cy.visit(`verify?address=${address}&signature=${signature}&email=${email}`);
-  });
-
-  it('shows the verify email button', () => {
-    cy.get('button').contains('Verify email');
-  });
-
   it('sends the correct request body to the backend API', () => {
+    cy.visit(`verify?address=${address}&signature=${signature}&email=${email}`);
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 }).as('verify');
     cy.get('button').click();
 
@@ -26,15 +19,15 @@ describe('Verify', () => {
   });
 
   it('shows the success message when the email is verified', () => {
+    cy.visit(`verify?address=${address}&signature=${signature}&email=${email}`);
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 });
-    cy.get('button').click();
     cy.get('p').contains('verified');
     cy.get('button').contains('Go back to Snapshot');
   });
 
   it('shows an error message when the verify request is failing', () => {
+    cy.visit(`verify?address=${address}&signature=${signature}&email=${email}`);
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 });
-    cy.get('button').click();
     cy.get('p').contains('error');
     cy.get('button').contains('Verify email');
   });
