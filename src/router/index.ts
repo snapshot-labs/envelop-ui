0ebeb9440716b11444c65e1a4ff99b713e9c9315
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-28 21:58:01 +0400

    fix: use hashtag route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Verify from '@/views/Verify.vue';
 import Unsubscribe from '@/views/Unsubscribe.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
