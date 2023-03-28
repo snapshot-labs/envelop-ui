bbb5f5d21732d44f691a8c6cefaf22792bfb33d7
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-28 22:10:06 +0400

    test: fix tests

--- cypress/e2e/unsubscribe.cy.ts
+++ cypress/e2e/unsubscribe.cy.ts
@@ -4,7 +4,7 @@ describe('Unsubscribe', () => {
 
   it('sends the correct request body to the backend API', () => {
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 }).as('unsubscribe');
-    cy.visit(`unsubscribe?signature=${signature}&email=${email}`);
+    cy.visit(`#/unsubscribe?signature=${signature}&email=${email}`);
 
     cy.get('@unsubscribe').its('request.body').should('deep.equal', {
       method: 'snapshot.unsubscribe',
@@ -17,20 +17,20 @@ describe('Unsubscribe', () => {
 
   it('shows the success message when the email is unsubscribed', () => {
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 });
-    cy.visit('unsubscribe');
+    cy.visit('#/unsubscribe');
     cy.get('p').contains('unsubscribed');
     cy.get('button').contains('Go back to Snapshot');
   });
 
   it('shows an error message when the unsubscribe request is failing', () => {
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 });
-    cy.visit('unsubscribe');
+    cy.visit('#/unsubscribe');
     cy.get('p').contains('error');
   });
 
   it('shows a button to retry when the unsubscribe request is failing', () => {
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 });
-    cy.visit('unsubscribe');
+    cy.visit('#/unsubscribe');
     cy.get('button').contains('Unsubscribe');
   });
 });
