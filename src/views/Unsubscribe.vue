c722896d2ae4d58b4ca839143dff17733044d938
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-11 23:40:02 +0400

    feat: manage email subscriptions from UNSUBSCRIBE page

--- src/views/Unsubscribe.vue
+++ src/views/Unsubscribe.vue
@@ -1,79 +1,134 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, computed, onMounted, Ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useSeoMeta } from '@vueuse/head';
-import MessageTitle from '@/components/MessageTitle.vue';
+import BasePage from '@/components/BasePage.vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import MessageBody from '@/components/MessageBody.vue';
+import MessageBox from '@/components/MessageBox.vue';
+import InputCheckbox from '@/components/InputCheckbox.vue';
 import RedirectButton from '@/components/RedirectButton.vue';
 import BaseButton from '@/components/BaseButton.vue';
 
 enum Status {
   UNKNOWN,
+  WAITING,
   SUCCESS,
   ERROR
 }
 
 let status = ref(Status.UNKNOWN);
 let loading = ref(false);
+let subscriptionsList: Ref<Record<string, Record<string, string>>> = ref({});
+let updatedSubscriptions: Ref<string[]> = ref([]);
 const route = useRoute();
 
 useSeoMeta({
-  title: 'Unsubscribe',
-  description: 'Unsubscribe from Snapshot weekly summary mailing list'
+  title: 'Manage email subscriptions',
+  description: 'Manage your snapshot mailing subscriptions'
 });
 
-function unsubscribe() {
-  loading.value = true;
+async function initForm() {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/subscriptionsList`);
+  status.value = Status.WAITING;
 
-  fetch(`${import.meta.env.VITE_API_URL}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      params: {
-        email: route.query.email,
-        signature: route.query.signature
-      },
-      method: 'snapshot.unsubscribe'
-    })
-  })
-    .then(response => {
-      loading.value = false;
-
-      if (response.status === 200) {
-        status.value = Status.SUCCESS;
-      } else {
-        status.value = Status.ERROR;
-      }
-    })
-    .catch(error => {
-      status.value = Status.ERROR;
-      loading.value = false;
-      console.log(error);
-    });
+  if (response.status === 200) {
+    subscriptionsList.value = await response.json();
+    updatedSubscriptions.value = Object.keys(subscriptionsList.value);
+  } else {
+    status.value = Status.ERROR;
+  }
 }
 
-onMounted(() => {
-  unsubscribe();
+function update() {
+  return unsubscribe(updatedSubscriptions.value);
+}
+
+function unsubscribeFromAll() {
+  return unsubscribe([]);
+}
+
+async function unsubscribe(subscriptions: string[]) {
+  loading.value = true;
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        params: {
+          email: route.query.email,
+          signature: route.query.signature,
+          subscriptions
+        },
+        method: 'snapshot.unsubscribe'
+      })
+    });
+
+    loading.value = false;
+    status.value = response.status === 200 ? Status.SUCCESS : Status.ERROR;
+  } catch (error) {
+    status.value = Status.ERROR;
+    loading.value = false;
+    console.log(error);
+  }
+}
+
+const subscriptionsListReady = computed(() => {
+  return Object.keys(subscriptionsList).length > 0;
 });
+
+onMounted(() => initForm());
 </script>
 
 <template>
-  <MessageTitle>Unsubscribe</MessageTitle>
-
-  <div v-if="status === Status.UNKNOWN">
-    <MessageBody>Please wait while we're unsubscribing your email.</MessageBody>
-  </div>
-  <div v-else-if="status === Status.SUCCESS">
-    <MessageBody>You have been unsubscribed from the Snapshot weekly summary.</MessageBody>
-    <RedirectButton />
-  </div>
-  <div v-else-if="status === Status.ERROR">
-    <MessageBody variant="danger">
-      An error occured while processing your request. Please try again, or
-      <a title="Contact the support" href="https://discord.snapshot.org/">contact the support</a>.
-    </MessageBody>
-    <BaseButton primary :loading="loading" @click="unsubscribe">Unsubscribe</BaseButton>
-  </div>
+  <BasePage title="Email preferences" :centered="false">
+    <div v-if="status === Status.UNKNOWN">
+      <MessageBody><LoadingSpinner :big="true" /></MessageBody>
+    </div>
+    <div v-else-if="status === Status.SUCCESS">
+      <MessageBody v-if="updatedSubscriptions.length === 0">
+        You have been unsubscribed from the Snapshot mailing list.
+      </MessageBody>
+      <MessageBody v-else>Your email subscription preferences have been updated.</MessageBody>
+      <RedirectButton class="mt-3" />
+    </div>
+    <div v-else class="text-left">
+      <div v-if="status === Status.ERROR">
+        <MessageBox variant="danger">
+          An error occured while processing your request. Please try again, or
+          <a title="Contact the support" href="https://discord.snapshot.org/">contact the support</a
+          >.
+        </MessageBox>
+      </div>
+      <form v-if="subscriptionsListReady">
+        <p class="mb-2">Select the email categories you wish to receive:</p>
+        <template v-for="(data, key) in subscriptionsList" :key="key">
+          <InputCheckbox v-model="updatedSubscriptions" :value="key" class="py-2">
+            <span class="subscription-name">{{ data.name }}</span>
+            <small class="subscription-description">{{ data.description }}</small>
+          </InputCheckbox>
+        </template>
+        <BaseButton primary :loading="loading" type="submit" class="mt-5" @click="update"
+          >Update preferences</BaseButton
+        >
+        <BaseButton :loading="loading" type="submit" class="mt-3" @click="unsubscribeFromAll"
+          >Unsubscribe from all</BaseButton
+        >
+      </form>
+    </div>
+  </BasePage>
 </template>
+
+<style scoped lang="scss">
+.subscription-name {
+  color: var(--heading-color);
+}
+
+.subscription-description {
+  line-height: 1.5;
+  display: block;
+}
+</style>
--- src/views/Verify.vue
+++ src/views/Verify.vue
@@ -2,8 +2,8 @@
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { useSeoMeta } from '@vueuse/head';
+import BasePage from '@/components/BasePage.vue';
 import BaseButton from '@/components/BaseButton.vue';
-import MessageTitle from '@/components/MessageTitle.vue';
 import MessageBody from '@/components/MessageBody.vue';
 import RedirectButton from '@/components/RedirectButton.vue';
 
@@ -19,60 +19,56 @@ const route = useRoute();
 
 useSeoMeta({
   title: 'Verify email',
-  description: 'Verify your email to confirm your subscription to Snapshot weekly summary'
+  description: 'Verify your email to confirm your subscription to Snapshot mailing list'
 });
 
-function verify() {
+async function verify() {
   loading.value = true;
 
-  fetch(`${import.meta.env.VITE_API_URL}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      params: {
-        email: route.query.email,
-        signature: route.query.signature,
-        address: route.query.address
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      method: 'snapshot.verify'
-    })
-  })
-    .then(response => {
-      loading.value = false;
-
-      if (response.status === 200) {
-        status.value = Status.SUCCESS;
-      } else {
-        status.value = Status.ERROR;
-      }
-    })
-    .catch(error => {
-      status.value = Status.ERROR;
-      loading.value = false;
-      console.log(error);
+      body: JSON.stringify({
+        params: {
+          email: route.query.email,
+          signature: route.query.signature,
+          address: route.query.address
+        },
+        method: 'snapshot.verify'
+      })
     });
+
+    loading.value = false;
+    status.value = response.status === 200 ? Status.SUCCESS : Status.ERROR;
+  } catch (error) {
+    status.value = Status.ERROR;
+    loading.value = false;
+    console.log(error);
+  }
 }
 
 onMounted(() => verify());
 </script>
 
 <template>
-  <MessageTitle>Verify your email</MessageTitle>
-  <div v-if="status === Status.UNKNOWN">
-    <MessageBody>Please wait while we are verifying your email address.</MessageBody>
-  </div>
-  <div v-else-if="status === Status.SUCCESS">
-    <MessageBody>Your email has been verified.</MessageBody>
-    <RedirectButton />
-  </div>
-  <div v-else>
-    <MessageBody variant="danger">
-      An error occured while verifying your email. Ensure your verification link is correct and try
-      again, or
-      <a title="Contact the support" href="https://discord.snapshot.org/">contact the support</a>.
-    </MessageBody>
-    <BaseButton primary :loading="loading" @click="verify">Verify email</BaseButton>
-  </div>
+  <BasePage :centered="true" title="Verify your email">
+    <div v-if="status === Status.UNKNOWN">
+      <MessageBody>Please wait while we are verifying your email address.</MessageBody>
+    </div>
+    <div v-else-if="status === Status.SUCCESS">
+      <MessageBody>Your email has been verified.</MessageBody>
+      <RedirectButton class="mt-3" />
+    </div>
+    <div v-else>
+      <MessageBody variant="danger">
+        An error occured while verifying your email. Ensure your verification link is correct and
+        try again, or
+        <a title="Contact the support" href="https://discord.snapshot.org/">contact the support</a>.
+      </MessageBody>
+      <BaseButton primary :loading="loading" @click="verify">Verify email</BaseButton>
+    </div>
+  </BasePage>
 </template>
