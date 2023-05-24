deb51ae0726a7c5339ce80cff54725f228f7a708
Wan <495709+wa0x6e@users.noreply.github.com>
2023-05-24 17:42:19 +0400

    Update src/views/Home.vue
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ import BrandLogo from '@/components/BrandLogo.vue';
 </script>
 
 <template>
-  <div :class="['max-w-screen-xs', 'xs:w-[420px]', 'p-5', 'py-6', { 'text-center': true }]">
+  <div class='max-w-screen-xs xs:w-[420px] p-5 py-6 text-center">
     <BrandLogo />
   </div>
 </template>
