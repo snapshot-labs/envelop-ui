3e40465224665e9eec4c94066597703a332856d2
Wan <495709+wa0x6e@users.noreply.github.com>
2023-05-29 20:37:09 +0400

    feat: separate `update` from `unsubscribe` (#8)
    * fix: add message when unsubscribe status is pending
    
    * feat: manage email subscriptions from UNSUBSCRIBE page
    
    * chore: update tests
    
    * chore: fix test on CI
    
    * chore: fix tests
    
    * Update src/views/Home.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * fix: fix invalid closing quote
    
    * feat: separate unsubscribe page from update
    
    * Update src/views/Unsubscribe.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * Update src/views/Update.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * Update src/views/Update.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * Update src/views/Unsubscribe.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * chore: code formatting
    
    * Update src/views/Unsubscribe.vue
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>
    
    * refactor: migrate `initForm` to use `useFetch`
    
    * fix: prevent double form submission
    
    * refactor: migrate `update` to use `useFetch`
    
    * chore: fix tests
    
    * refactor: migrate verify vue to use `useFetch`
    
    ---------
    
    Co-authored-by: Dmytro Tolok <tolok.d@gmail.com>

--- cypress/e2e/unsubscribe.cy.ts
+++ cypress/e2e/unsubscribe.cy.ts
@@ -1,88 +1,38 @@
 describe('Unsubscribe', () => {
   const email = 'test@snapshot.org';
   const signature = '0x0000';
+  const subscriptions = [];
 
   it('sends the correct request body to the backend API', () => {
-    cy.intercept('GET', `${Cypress.env('VITE_API_URL')}/subscriptionsList`, {
-      statusCode: 200,
-      body: { summary: { name: 'Summary', description: 'Summary description' } }
-    }).as('subscriptionsList');
     cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 }).as('unsubscribe');
     cy.visit(`#/unsubscribe?signature=${signature}&email=${email}`);
-    cy.get('[data-test="btn-submit"]').click();
-    cy.get('@unsubscribe')
-      .its('request.body')
-      .should('deep.equal', {
-        method: 'snapshot.unsubscribe',
-        params: {
-          email,
-          signature,
-          subscriptions: ['summary']
-        }
-      });
+
+    cy.get('@unsubscribe').its('request.body').should('deep.equal', {
+      method: 'snapshot.unsubscribe',
+      params: {
+        email,
+        signature,
+        subscriptions
+      }
+    });
   });
 
-  it('shows the unsubscribe success message when the email is unsubscribed', () => {
-    cy.intercept('GET', `${Cypress.env('VITE_API_URL')}/subscriptionsList`, {
-      statusCode: 200,
-      body: { summary: { name: 'Summary', description: 'Summary description' } }
-    }).as('subscriptionsList');
-    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 }).as('unsubscribe');
-    cy.visit(`#/unsubscribe?signature=${signature}&email=${email}`);
-    cy.get('form input:first-child()').click();
-    cy.get('[data-test="btn-submit"]').click();
-    cy.get('[data-test="message-success-unsubscribe"]').should('exist');
-    cy.get('[data-test="btn-redirect"]').should('exist');
-    cy.get('@unsubscribe')
-      .its('request.body')
-      .should('deep.equal', {
-        method: 'snapshot.unsubscribe',
-        params: {
-          email,
-          signature,
-          subscriptions: []
-        }
-      });
-  });
-
-  it('shows the update success message when the preferences are updated', () => {
-    cy.intercept('GET', `${Cypress.env('VITE_API_URL')}/subscriptionsList`, {
-      statusCode: 200,
-      body: { summary: { name: 'Summary', description: 'Summary description' } }
-    }).as('subscriptionsList');
-    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 }).as('unsubscribe');
-    cy.visit(`#/unsubscribe?signature=${signature}&email=${email}`);
-    cy.get('[data-test="btn-submit"]').click();
-    cy.get('[data-test="message-success-update"]').should('exist');
-    cy.get('[data-test="btn-redirect"]').should('exist');
-    cy.get('@unsubscribe')
-      .its('request.body')
-      .should('deep.equal', {
-        method: 'snapshot.unsubscribe',
-        params: {
-          email,
-          signature,
-          subscriptions: ['summary']
-        }
-      });
-  });
-
-  it('shows an error message when the unable to build the subscriptions list', () => {
-    cy.intercept('GET', `${Cypress.env('VITE_API_URL')}/subscriptionsList`, { statusCode: 500 });
+  it('shows the success message when the email is unsubscribed', () => {
+    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 200 });
     cy.visit('#/unsubscribe');
-    cy.get('[data-test="message-error"]').should('exist');
-    cy.get('[data-test="form"]').should('not.exist');
+    cy.get('[data-test="message-success"]').should('exist');
+    cy.get('[data-test="btn-redirect"]').should('exist');
   });
 
   it('shows an error message when the unsubscribe request is failing', () => {
-    cy.intercept('GET', `${Cypress.env('VITE_API_URL')}/subscriptionsList`, {
-      statusCode: 200,
-      body: { summary: { name: 'Summary', description: 'Summary description' } }
-    }).as('subscriptionsList');
-    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 }).as('post');
+    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 });
     cy.visit('#/unsubscribe');
-    cy.get('[data-test="btn-submit"]').click();
     cy.get('[data-test="message-error"]').should('exist');
-    cy.get('[data-test="form"]').should('exist');
+  });
+
+  it('shows a button to retry when the unsubscribe request is failing', () => {
+    cy.intercept('POST', Cypress.env('VITE_API_URL'), { statusCode: 500 });
+    cy.visit('#/unsubscribe');
+    cy.get('[data-test="btn-submit"]').should('exist');
   });
 });
---
+++ src/views/Update.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { ref, computed, onMounted, Ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useSeoMeta } from '@vueuse/head';
+import { useFetch } from '@vueuse/core';
+import BasePage from '@/components/BasePage.vue';
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import MessageBody from '@/components/MessageBody.vue';
+import MessageBox from '@/components/MessageBox.vue';
+import InputCheckbox from '@/components/InputCheckbox.vue';
+import RedirectButton from '@/components/RedirectButton.vue';
+import BaseButton from '@/components/BaseButton.vue';
+
+enum Status {
+  UNKNOWN,
+  INITIATED,
+  SUCCESS,
+  ERROR
+}
+
+let status = ref(Status.UNKNOWN);
+let loading = ref(false);
+let subscriptionsList: Ref<Record<string, Record<string, string>>> = ref({});
+let updatedSubscriptions: Ref<string[]> = ref([]);
+const route = useRoute();
+
+useSeoMeta({
+  title: 'Manage email preferences',
+  description: 'Manage your Snapshot mailing preferences'
+});
+
+async function initForm() {
+  status.value = Status.INITIATED;
+  const fetch = useFetch(`${import.meta.env.VITE_API_URL}/subscriptionsList`)
+    .get()
+    .json();
+
+  // eslint-disable-next-line vue/no-ref-as-operand
+  loading = fetch.isFetching;
+  // eslint-disable-next-line vue/no-ref-as-operand
+  subscriptionsList = fetch.data;
+
+  fetch.onFetchError(() => {
+    status.value = Status.ERROR;
+  });
+
+  fetch.onFetchResponse(() => {
+    updatedSubscriptions.value = Object.keys(subscriptionsList.value);
+  });
+}
+
+async function update() {
+  const fetch = useFetch(import.meta.env.VITE_API_URL, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .post(() => ({
+      params: {
+        email: route.query.email,
+        signature: route.query.signature,
+        subscriptions: updatedSubscriptions.value
+      },
+      method: 'snapshot.update'
+    }))
+    .json();
+
+  // eslint-disable-next-line vue/no-ref-as-operand
+  loading = fetch.isFetching;
+
+  fetch.onFetchResponse(() => {
+    status.value = Status.SUCCESS;
+  });
+
+  fetch.onFetchError(() => {
+    status.value = Status.ERROR;
+  });
+}
+
+const subscriptionsListReady = computed(() => {
+  return subscriptionsList.value && Object.keys(subscriptionsList.value).length > 0;
+});
+
+onMounted(initForm);
+</script>
+
+<template>
+  <BasePage title="Email preferences" :centered="false">
+    <div v-if="status === Status.UNKNOWN">
+      <MessageBody><LoadingSpinner :big="true" /></MessageBody>
+    </div>
+    <div v-else-if="status === Status.SUCCESS">
+      <MessageBody data-test="message-success">
+        Your email subscription preferences have been updated.
+      </MessageBody>
+      <RedirectButton class="mt-3" />
+    </div>
+    <div v-else class="text-left">
+      <div v-if="status === Status.ERROR">
+        <MessageBox data-test="message-error" variant="danger">
+          An error occured while processing your request. Please try again, or
+          <a title="Contact the support" href="https://discord.snapshot.org/">
+            contact the support
+          </a>
+          .
+        </MessageBox>
+      </div>
+      <form v-if="subscriptionsListReady" data-test="form" @submit.prevent="update">
+        <p class="mb-2">Select the email categories you wish to receive:</p>
+        <template v-for="(data, key) in subscriptionsList" :key="key">
+          <InputCheckbox v-model="updatedSubscriptions" :value="key" class="py-2">
+            <span class="subscription-name">{{ data.name }}</span>
+            <small class="subscription-description">{{ data.description }}</small>
+          </InputCheckbox>
+        </template>
+        <BaseButton primary :loading="loading" data-test="btn-submit" type="submit" class="mt-5">
+          Update preferences
+        </BaseButton>
+      </form>
+    </div>
+  </BasePage>
+</template>
+
+<style scoped lang="scss">
+.subscription-name {
+  color: var(--heading-color);
+}
+
+.subscription-description {
+  line-height: 1.5;
+  display: block;
+}
+</style>
