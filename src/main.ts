f8b82422f2f6251749e3dfcc11821540897088e4
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-01 14:29:22 +0900

    Revert "chore: add sentry error logging"
    This reverts commit 6a9bd923e2a98a529bb70572038787e899575ae5.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import { createHead } from '@vueuse/head';
-import * as Sentry from '@sentry/vue';
 
 import App from '@/App.vue';
 import router from '@/router';
@@ -9,20 +8,6 @@ import '@/style.scss';
 
 const app = createApp(App);
 const head = createHead();
-
-Sentry.init({
-  app,
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
-    }),
-    new Sentry.Replay()
-  ],
-  tracesSampleRate: 0.5,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0
-});
 
 app.use(router);
 app.use(head);
