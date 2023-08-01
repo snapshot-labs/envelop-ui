6b82e1a29f3d9562eaf1ebc9cabbf1e6794743ab
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-01 16:17:14 +0900

    chore: build and upload sourcemaps to Sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    sourcemap: true // Source map generation must be turned on
+  },
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
