723aaa6839c0ffbc536596340923107aa775defa
wa0x6e <495709+wa0x6e@users.noreply.github.com>
2026-02-07 11:52:57 -0500

    fix: update branding

--- src/components/BrandLogo.vue
+++ src/components/BrandLogo.vue
@@ -2,37 +2,8 @@
   <div class="brand-logo">
     <img
       alt="Snapshot logo"
-      src="~@/assets/logo.png"
-      class="theme-light max-h-7"
-    />
-    <img
-      alt="Snapshot logo"
-      src="~@/assets/logo-white.png"
-      class="theme-dark max-h-7"
+      src="~@/assets/logo.svg"
+      class="max-h-7 rounded-md inline-block"
     />
   </div>
 </template>
-
-<style scoped lang="scss">
-.brand-logo {
-  img {
-    @apply inline-block;
-  }
-
-  .theme-dark {
-    @apply hidden;
-  }
-}
-
-@media (prefers-color-scheme: dark) {
-  .brand-logo {
-    .theme-dark {
-      @apply inline-block;
-    }
-
-    .theme-light {
-      @apply hidden;
-    }
-  }
-}
-</style>
